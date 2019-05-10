7f0cc20dcf88f0b8eecd565139df1b4ed575fe4f
Maleen Abeydeera <maleen@mit.edu>
2019-05-10 12:16:58 -0400

    root README

--- aws_setup.sh
+++ aws_setup.sh
@@ -1,2 +1,2 @@
 (cd ~/swarm/aws-fpga; source hdk_setup.sh)
-(cd ~/swarm/aws-fpga/hdl/cl/developer_designs/cl_swarm; export CL_DIR=$(pwd))
+(cd ~/swarm/aws-fpga/hdk/cl/developer_designs/cl_swarm; export CL_DIR=$(pwd))
