1ea1e00d64b91eed3a7c4fac8265e0df408b023f
Maleen Abeydeera <maleen@mit.edu>
2019-07-07 00:26:45 -0400

    [task_unit] log args[95:64]

--- hdk/cl/developer_designs/cl_swarm/design/task_unit.sv
+++ hdk/cl/developer_designs/cl_swarm/design/task_unit.sv
@@ -1434,10 +1434,13 @@ if (TASK_UNIT_LOGGING[TILE_ID]) begin
         log_word.enq_task_coal_child.ready = task_enq_ready;
         log_word.enq_task_coal_child.slot  = next_free_tq_slot;
         log_word.enq_task_coal_child.epoch_1 = epoch[next_free_tq_slot];
-        if (alt_log_word & ARG_WIDTH == 64) begin
+        if (alt_log_word & ARG_WIDTH >= 32) begin
 		
            log_word.deq_locale = task_enq_data.args[31:0] ;
            log_word.deq_ts   = task_enq_data.args[63:32];
+           if (alt_log_word & ARG_WIDTH >= 64) begin
+              log_word.gvt_tb = task_enq_data.args[95:64];
+           end
         end
         log_valid = 1;
      end else if (coal_child_valid & coal_child_ready) begin
