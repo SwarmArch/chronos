b8ead658c2edd70b117491b8e1c10d1367f6c0d2
Maleen Abeydeera <maleen@mtl.mit.edu>
2019-11-25 13:28:27 -0500

    [validation] skeleton of remaining scripts

---
+++ hdk/cl/developer_designs/cl_chronos/validation/inputs/fetch_inputs.py
@@ -0,0 +1,5 @@
+## Downloads inputs from some specified locations
+# sssp - USA.gr.sssp
+# des  - csa32
+# astar - ddd
+# maxflow - genrmf37
---
+++ hdk/cl/developer_designs/cl_chronos/validation/scripts/run.py
@@ -0,0 +1,6 @@
+## Input
+## 1. agfi_list.txt, 
+## 2. experiments.txt where each line is [app-name, app-tag, n_cores, n_threads] 
+
+## Runs the specified experiments. The output of each will be placed in
+## validation/runs/<date>/app_tiles_threads.txt
