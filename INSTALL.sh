f93a2270ea1f8ee9b9c2fc0c6db0cb106ef78751
Maleen Abeydeera <maleen@mit.edu>
2019-12-03 22:07:24 -0500

    INSTALL.sh

---
+++ INSTALL.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -e aws-fpga ] 
+then
+   echo "aws-fpga already exists. Not cloning.." 
+else
+   git clone https://github.com/aws/aws-fpga.git
+fi 
+
+if [ -e aws-fpga/hdk/cl/developer_designs/cl_chronos ] 
+then
+   echo "cl_chronos symling already exists. Not creating.." 
+else
+   ln -s cl_chronos aws-fpga/hdk/cl/developer_designs/cl_chronos 
+   echo "Creating symlink aws_fpga/hdk/cl/developer_designs/cl_chronos" 
+fi 
